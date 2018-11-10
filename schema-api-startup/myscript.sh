e2db3a01eff33ed285a72e63a8e16503c69bc24f
Martijn Koster <mak-github@greenhills.co.uk>
2018-11-10 16:45:59 +0000

    Make sure the dir exists

--- schema-api-startup/myscript.sh
+++ schema-api-startup/myscript.sh
@@ -62,7 +62,7 @@ else
     stop-local-solr
 
     # move the core to "mycores" so users can mount a directory there
-    mv "/opt/solr/server/solr/$core_name" /opt/solr/server/solr/mycores
+    mv "/opt/solr/server/solr/$core_name" /opt/solr/server/solr/mycores/
 
     touch $sentinel
 fi
